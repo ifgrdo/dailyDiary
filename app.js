35835f132e84d11282fa1dbee00e2cb20625f9d6
ifgrdo <ifgrd.26@gmail.com>
2023-02-01 22:48:48 +0000

    add mongoose

--- app.js
+++ app.js
@@ -1,29 +1,43 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
+
 
 const homeStartingContent = "";
 const aboutContent = "";
 const contactContent = "";
 
-const postArray = [];
-
 const app = express();
+
 app.set('view engine', 'ejs');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb://0.0.0.0:27017/diary');
+
+
+const postSchema = mongoose.Schema ({
+    title: String,
+    body: String
+})
+
+const Post = mongoose.model("Post", postSchema);
 
 
 app.get("/", (req, res) => {
-    res.render("home", {
-        homeStartingContent: homeStartingContent,
-        postArray: postArray,
-        _: _
-    });
+    Post.find({}, (err, posts) => {
+        res.render("home", {
+            homeStartingContent: homeStartingContent,
+            posts: posts,
+            _: _
+        });
+    })
 })
+
 
 app.get("/about", (req, res) => {
     res.render("about", {
@@ -31,47 +45,65 @@ app.get("/about", (req, res) => {
     });
 })
 
+
 app.get("/contact", (req, res) => {
     res.render("contact", {
         contactContent: contactContent
     });
 })
 
+
 app.get("/compose", (req, res) => {
     res.render("compose");
 })
+
 
 app.get("/posts/:name", (req, res) => {
 
     const requestedName = _.lowerCase(req.params.name);
 
-    postArray.forEach((post) => {
-        const currentName = _.lowerCase(post.title);
+    Post.find({}, (err, posts) => {
 
-        if (currentName == requestedName) {
-            res.render("post", {
-                title: post.title,
-                body: post.body
-            });
-        } 
+        posts.forEach((post) => {
+
+            const currentName = _.lowerCase(post.title);
+
+            if (currentName == requestedName) {
+                res.render("post", {
+                    title: post.title,
+                    body: post.body
+                });
+            } 
+        })
     })
 })
 
 
-
-
 app.post("/compose", (req, res) => {
-    const post = {
+    
+    const newPost = new Post ({
         title: req.body.title,
         body: req.body.post
-    }
+    })
 
-    postArray.push(post);
+    newPost.save();
 
     res.redirect("/");
 })
 
 
+app.post("/delete", (req, res) => {
+
+    const postID = req.body.postID;
+
+    Post.deleteOne( {_id: postID} , (err, foundList) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect("/");
+        }
+    })
+})
 
 
 app.listen(3000, function () {
